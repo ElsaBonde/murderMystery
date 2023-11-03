10bc2c4e406ffcf0f9df62bec30f9c3708f13a3e
Elsa Bonde <elsasofiabonde@gmail.com>
2023-11-03 15:00:00 +0100

    new startpage among other things

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  renderScene();
+  startGame();
 }
 
-function renderScene() { //skapar variabler som hämtar de föränderliga elementen i js så som text och knappar, bakgrundsbild
+function startGame() {
+  //hämtar med nedstående klasser (till höger) och ger dessa variabelnamn i js 
+  const startH1 = document.querySelector(".startH1");
+  const startH3 = document.querySelector(".startH3");
+  const startH4 = document.querySelector(".startH4");
+  const startP = document.querySelectorAll(".startP");
+
+  startH1.textContent = start.h1; //hämtar hem alla textelementen i objektet för aktiv scen och visar dessa
+  startH3.textContent = start.h3;
+  startH4.textContent = start.h4;
+
+  //hämtar hem alla startP element som finns i objektet genom att loopa igenom och öka med ett för varje gång det finns ett "rätt" svar
+  for (let i = 0; i < startP.length; i++) {
+    startP[i].textContent = start["p" + (i + 1)];
+  }
+
+  //hämtar hem bodyn och ger den namnet startPage för att sedan byta bakgrundbilden till den som finns i objektet start
+  const startPage = document.querySelector("body");
+  startPage.style.backgroundImage = start.backgroundImage;
+
+  //skapar variabelnamn till startGameButton
+  const startGameButton = document.getElementById("startGameButton");
+
+  //skapar en anonym funktion som anropar funktionen renderScene
+  startGameButton.addEventListener("click", function () {
+
+     //hämtar alla element i klassen startRoom
+  const startRoomElements = document.querySelectorAll(".startRoom");
+
+  //loopar igenom och ta bort varje element eftersom dom bara ska vara med på första sidan
+  startRoomElements.forEach(function(element) {
+    element.remove();
+  });
+    renderScene();
+  })
+}
+
+
+function renderScene() {
+  //skapar variabler som hämtar de föränderliga elementen i js så som text och knappar, bakgrundsbild
   const text = document.getElementById("text");
   const item1 = document.getElementById("item-1");
   const item2 = document.getElementById("item-2");
-  const body = document.getElementById("body");
+  const body = document.body;
+
+
+  text.style.display = "block";
+  item1.style.display = "block";
+  item2.style.display = "block";
+  body.style.display = "contenst";
 
   const scene = scenes[activeSceneIndex]; //skapar variabel som hämtar hem den aktiva scenen spelaren befinner sig på i arrayen scenes
 
@@ -16,7 +61,6 @@ function renderScene() { //skapar variabler som hämtar de föränderliga elemen
   item1.textContent = scene.item1.text;
   item2.textContent = scene.item2.text;
   body.style.backgroundImage = scene.backgroundImage; //ändrar style för bodyn backgrundbild utefter aktivt element
-  
 
   //när man klickar på item1 (vänster knapp) går man till scenen som objektets egenskap kallart på i index i arrayen för scener
   item1.onclick = function () {
@@ -25,6 +69,10 @@ function renderScene() { //skapar variabler som hämtar de föränderliga elemen
   item2.onclick = function () {
     goNextScene(scene.item2.nextSceneIndex);
   };
+
+  console.log(text.style.display);
+  console.log(item1.style.display);
+  console.log(item2.style.display);
 }
 
 //gör om det aktiva sceneindex till sidan man nu befinner sig, är man ex på scen med indexvärde 1 (vardagsrummet) och trycker på item två (högra knappen) så komemr man iom objektets egenskaper till scenen med indexvärde två (köket)
--- scenes.js
+++ scenes.js
@@ -1,4 +1,5 @@
 let activeSceneIndex = 0;
+
 const scenes = [
   { //scen 1 - veranda
     text: "Pick up the items you can see to put them in your inventory. Maybe they'll come in handy later. After that you might want to go inside the house?",
@@ -13,7 +14,7 @@ const scenes = [
     backgroundImage: 'url("src/porch.jpg")'
   },
   { //scen två - vardagsrum
-    text: "It looks like there are two things to add to your inventory!",
+    text: "It looks like there are two things to add to your inventory! Collect them, they may come in handy later on..",
     item1: {
       text: "Go to porch",
       nextSceneIndex: 0,
@@ -35,10 +36,10 @@ const scenes = [
       text: "Go to bedroom",
       nextSceneIndex: 3,
     },
+    backgroundImage: 'url("src/kitchen.jpg")'
   },
   { //scen fyra - sovrum
-    background: 'url("bedroom.jpg")',
-    text: "Call 911!",
+    text: "Call 911! Quick, click the right item in your inventory to call the police.",
     item1: {
       text: "Go to kitchen",
       nextSceneIndex: 2,
@@ -47,17 +48,43 @@ const scenes = [
       text: "Go to bathroom",
       nextSceneIndex: 4,
     },
+    backgroundImage: 'url("src/bedroom.jpg")'
   },
-  { //scen fem - badrum
-    background: 'url("bathroom.jpg")',
+  { //scen fem - badrum släckt
     text: "Its so dark here, maybe you should put the lamp on?",
     item1: {
       text: "Go to bedroom",
       nextSceneIndex: 3,
     },
     item2: {
-      text: "Go to closet",
+      text: "Turn the light on",
       nextSceneIndex: 5,
     },
-  }, //sen kanske scen 6 - gaderob, där mördaren är
+    backgroundImage: 'url("src/bathroomDark.jpg")'
+  },
+  { //scen fem - badrum tänt
+    text: "AHHHH!! Quickly make a choice about what you want to attack him with.",
+    item1: {
+      text: "Run for your life",
+      nextSceneIndex: 0,
+    },
+    item2: {
+      text: "Attack",
+      nextSceneIndex: 5,
+    },
+    backgroundImage: 'url("src/bathroom.jpg")'
+  },
 ];
+
+
+
+//startpage content
+const start = {
+  h1: "MURDER MYSTERY",
+  h3: "You're a private investigator on your way to a client who has experienced discomfort in her own house and is afraid that she has a stalker.. She called you screaming for help.",
+  h4: "How to play the game:",
+  p1: "1. Search the environment around you and collect objects.",
+  p2: "2. Use the items you have previously found when needed in the game, for example to get into new locations or to use to create new items.",
+  p3: "3. Have fun while trying to solve the case!",
+  backgroundImage: 'url("src/backgroundimg.jpg")'
+}
